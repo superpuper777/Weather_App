e64372ef9aa5d66af781a9b770e959c4c3e9e9b2
Alena Budnik <alena.budnik@effective-soft.com>
2021-06-24 15:46:57 +0300

    feat: add styled-components and react-icons, create structure of the project, add styles to header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import './App.css';
+import Header from './components/Header';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
-  );
+  return <Header />;
 }
 
 export default App;
