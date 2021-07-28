b90c2de735bc3d16644403633949765d78e55f95
Alena Budnik <alena.budnik@effective-soft.com>
2021-07-28 17:18:42 +0300

    feat: create store

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 import './App.css';
 import RouterComponent from 'router';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const weather = useSelector((state) => state.weather);
+
+  const forecast = useSelector((state) => state.forecast);
+
+  console.log(weather, forecast);
+
   return <RouterComponent />;
 }
 
