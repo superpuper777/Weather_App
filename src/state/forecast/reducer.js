39a6d272da0b22ffeeb8c8385fbe311e29d56de4
Alena Budnik <alena.budnik@effective-soft.com>
2021-08-09 14:20:38 +0300

    refactor: change forecast reducer and related components

--- src/state/forecast/reducer.js
+++ src/state/forecast/reducer.js
@@ -1,13 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-// import {
-//   FETCH_FORECAST_REQUEST,
-//   FETCH_FORECAST_SUCCESS,
-//   FETCH_FORECAST_ERROR,
-// } from './action-creators';
-
 const initialState = {
-  forecast: null,
+  forecast: [],
   isLoading: false,
   isError: false,
 };
@@ -16,26 +10,20 @@ const forecastSlice = createSlice({
   name: 'forecast',
   initialState,
   reducers: {
-    fetchForecastRequest() {
-      return {
-        forecast: null,
-        isLoading: true,
-        isError: false,
-      };
+    fetchForecastRequest: (state) => {
+      state.forecast = null;
+      state.isLoading = true;
+      state.isError = false;
     },
-    fetchForecastSuccess(action) {
-      return {
-        forecast: action.payload,
-        isLoading: false,
-        isError: false,
-      };
+    fetchForecastSuccess: (state, action) => {
+      state.forecast = action.payload;
+      state.isLoading = false;
+      state.isError = false;
     },
-    fetchForecastError() {
-      return {
-        forecast: null,
-        isLoading: false,
-        isError: true,
-      };
+    fetchForecastError(state) {
+      state.forecast = null;
+      state.isLoading = false;
+      state.isError = true;
     },
   },
 });
@@ -44,30 +32,3 @@ export default forecastSlice.reducer;
 
 export const { fetchForecastRequest, fetchForecastSuccess, fetchForecastError } =
   forecastSlice.actions;
-
-// const forecastReducer = (state = initialState, action) => {
-//   switch (action.type) {
-//     case FETCH_FORECAST_REQUEST:
-//       return {
-//         forecast: null,
-//         isLoading: true,
-//         isError: false,
-//       };
-//     case FETCH_FORECAST_SUCCESS:
-//       return {
-//         forecast: action.payload,
-//         isLoading: false,
-//         isError: false,
-//       };
-//     case FETCH_FORECAST_ERROR:
-//       return {
-//         forecast: null,
-//         isLoading: false,
-//         isError: true,
-//       };
-//     default:
-//       return state;
-//   }
-// };
-
-// export default forecastReducer;
